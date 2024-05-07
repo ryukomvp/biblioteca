66df7b633864a60678059c1336b7aadca2afb41e
ryukomvp <daniel123hernandez15@gmail.com>
2024-05-07 13:51:32 -0600

    Creación del proyecto
    Agregue los archivos del proyecto en SharpDevelop

---
+++ biblioteca/biblioteca/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: danie
+ * Date: 07/05/2024
+ * Time: 13:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace biblioteca
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
